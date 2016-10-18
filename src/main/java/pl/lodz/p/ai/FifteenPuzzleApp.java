c783bd11293ec59f7a2cabb35227d28f6cc49676
mgapsa <gapsamikolaj@gmail.com>
2016-10-18 10:46:56 +0200

    added comments

--- src/main/java/pl/lodz/p/ai/FifteenPuzzleApp.java
+++ src/main/java/pl/lodz/p/ai/FifteenPuzzleApp.java
@@ -13,20 +13,33 @@ import pl.lodz.p.ai.utility.file.QuizReader;
 import pl.lodz.p.ai.utility.puzzle.FifteenPuzzleStateGenerator;
 
 /**
- * Hello world!
+ * BFS: 
+ * 1. Create graph first(stack,iterative), then search - creating graph of a given depth
+ *  using BFS and then search using BFS(stack, iterative)
+ * 2. During creation of a graph we search for the solution - BFS both cases (stack,iterative)
+ * DFS:
+ * 1. Create graph first(stack, iterative), then search - creating graph of a given depth
+ *  using BFS and then search using DFS - recursive
+ * 2. During creation of a graph we search for the solution - DFS both cases - can look
+ * for worse solution as we are using optimisation - iterative
+ * Best-first strategy:
+ * A* strategy:
+ * SMA*:
  */
 public class FifteenPuzzleApp {
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
 	
-	    Array2D array2D = QuizReader.read("solution.json");
-	
-	    PuzzleState puzzleStateJanusz = new PuzzleState.PuzzleStateBuilder(array2D).build();
-	
-	    PuzzleState puzzleState = FifteenPuzzleStateGenerator.generateRandomState(puzzleStateJanusz, 17);
-	    puzzleState.setLastMoveToNull();
-	    System.out.println(puzzleState.toString());
-        
+      //read json file - which is solution
+      Array2D array2D = QuizReader.read("solution.json");
+	  //create puzzle state - with solution
+      PuzzleState puzzleStateJanusz = new PuzzleState.PuzzleStateBuilder(array2D).build();
+	  //generate random unsolved(probably) state
+      PuzzleState puzzleState = FifteenPuzzleStateGenerator.generateRandomState(puzzleStateJanusz, 17);
+      puzzleState.setLastMoveToNull();
+      System.out.println("Random state:");
+      System.out.println(puzzleState.toString());
+      
+      //create graph(using BFS strategy) used by DFS and BFS alghorithms
       Graph grafJanusz = new Graph.GraphBuilder(puzzleState).depth(17).build();
       
       ISearch searchAlg = new DFSSearch(grafJanusz);
@@ -34,18 +47,17 @@ public class FifteenPuzzleApp {
       
       if(node!= null)
       {
-      	System.out.println("EKSTRA");
+      	System.out.println("DFSSearch solution found:");
       	System.out.println(node.getDepth());
       	System.out.println(node.getPuzzleState().toString());
       }
-      //1:2:3:4:5:6:7:8:13:0:10:11:14:9:15:12:
       
       searchAlg = new BFSSearch(grafJanusz);
       node = searchAlg.SearchSolution();
       
       if(node!= null)
       {
-      	System.out.println("EKSTRA2");
+      	System.out.println("BFSSearch solution found:");
       	System.out.println(node.getDepth());
       	System.out.println(node.getPuzzleState().toString());
       }
@@ -54,21 +66,22 @@ public class FifteenPuzzleApp {
         Node dfsSolution = new DFSGraphBuilderAndSolver.GraphBuilderAndSolver(puzzleState).depth(17).buildAndSolve();
         if(dfsSolution != null)
         {
-        	System.out.println("EKSTRA3");
+        	System.out.println("DFS iterative:");
         	System.out.println(dfsSolution.getDepth());
         	System.out.println(dfsSolution.getPuzzleState().toString());   	
         }
         
-	    System.out.println("UWAGA, BFS");
         Node bfsSolution = new BFSGraphBuilderAndSolver.GraphBuilderAndSolver(puzzleState).depth(17).buildAndSolve();
         if(bfsSolution != null)
         {
-        	System.out.println("EKSTRA4");
+        	System.out.println("BFS optimized:");
         	System.out.println(bfsSolution.getDepth());
         	System.out.println(bfsSolution.getPuzzleState().toString());   	
         }
         
-        System.out.println("porobione");
+        System.out.println("done");
+        
+        
         
         //BFS zoptymalizowany - jest podczas budowy szuka od razu - lepiej sie hcyba nie
         //da, zawsze znajdzie najszybsze rozwiazanie
