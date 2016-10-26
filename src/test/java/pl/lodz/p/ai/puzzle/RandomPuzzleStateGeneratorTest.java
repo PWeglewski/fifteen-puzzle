d2e0c84f80d6f024bfe930a8603fe67e80237314
Piotr Weglewski <190160@edu.p.lodz.pl>
2016-10-26 14:31:08 +0200

    Added random generator empty skeleton

---
+++ src/test/java/pl/lodz/p/ai/puzzle/RandomPuzzleStateGeneratorTest.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.ai.puzzle;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Piotr on 2016-10-26.
+ */
+public class RandomPuzzleStateGeneratorTest {
+    @Test
+    public void generateRandomQuiz() throws Exception {
+        PuzzleState actualState = RandomPuzzleStateGenerator.generateRandomQuiz(1);
+    }
+
+}
